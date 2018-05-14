03fd92128986e4910af7828c57acd217ba6ec596
Humad <humad@live.com>
2018-05-14 12:23:24 -0400

    updated user add/delete functions

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-'use strict';
+"use strict";
 
 // Modules
-const express = require('express');
-const bodyParser = require('body-parser');
-const request = require('request');
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
 const app = express();
 
 // App setup
-app.set('port', (process.env.PORT || 8000));
+app.set("port", (process.env.PORT || 8000));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
@@ -17,25 +17,25 @@ const mLabUri = "mongodb://" + process.env.writerId +
 ":" + process.env.writerPass + "@ds157641.mlab.com:57641/motivate-bot";
 
 // Start server
-app.listen(app.get('port'), function() {
-	console.log('running on port', app.get('port'));
+app.listen(app.get("port"), function() {
+	console.log("running on port", app.get("port"));
 });
 
 // Home
-app.get('/', function (req, res) {
-	res.send('Hello world!');
+app.get("/", function (req, res) {
+	res.send("Hello world!");
 });
 
 // Facebook verification
-app.get('/webhook/', function (req, res) {
-	if (req.query['hub.verify_token'] === 'somethingsomethingtokenstuff') {
-		res.send(req.query['hub.challenge']);
+app.get("/webhook/", function (req, res) {
+	if (req.query["hub.verify_token"] === "somethingsomethingtokenstuff") {
+		res.send(req.query["hub.challenge"]);
 	}
-	res.send('Error, wrong token');
+	res.send("Error, wrong token");
 });
 
 // Message receiver
-app.post('/webhook/', function(req, res) {
+app.post("/webhook/", function(req, res) {
     var messaging_events = req.body.entry[0].messaging;
     for (var i = 0; i < messaging_events.length; i++) {
         var event = req.body.entry[0].messaging[i];
@@ -47,7 +47,9 @@ app.post('/webhook/', function(req, res) {
             } else if (text === "stop" || text === "remove") {
 				removeUserFromDB(sender);
 			} else {
-				sendTextMessage(sender, text + "!");
+				getQuote(function(quote) {
+					sendTextMessage(sender, quote);
+				});
 			}
         }
     }
@@ -59,24 +61,24 @@ app.post('/webhook/', function(req, res) {
 function sendTextMessage(sender, text) {
     var messageData = {text: text};
     request({
-	    url: 'https://graph.facebook.com/v2.6/me/messages',
+	    url: "https://graph.facebook.com/v2.6/me/messages",
 	    qs: {access_token: token},
-	    method: 'POST',
+	    method: "POST",
 		json: {
 		    recipient: {id: sender},
 			message: messageData,
+			tag: "NON_PROMOTIONAL_SUBSCRIPTION"
 		}
 	}, function(error, response, body) {
 		if (error) {
-		    console.log('Error:', error);
+		    console.log("Error:", error);
 		} else if (response.body.error) {
-		    console.log('Error: ', response.body.error);
+		    console.log("Error: ", response.body.error);
 	    }
     });
 }
 
 function addUserToDB(sender) {
-
 	mongo.connect(mLabUri, function(err, client){
         if (err){
             throw err;
@@ -92,7 +94,7 @@ function addUserToDB(sender) {
 							console.log(err);
 						}
 						if (res) {
-							console.log(res);
+							console.log("Added user");
 						}
 						client.close();
 					});
@@ -108,32 +110,39 @@ function addUserToDB(sender) {
 }
 
 function removeUserFromDB(sender) {
-	// mongo.connect(mLabUri, function(err, client){
-    //     if (err){
-    //         throw err;
-    //         res.end(err);
-    //     } else {
-	// 		var db = client.db("motivate-bot");
-    //         db.collection("recipients").findOne({"sender" : sender}, function(err, result){
-	// 			if (result === null) {
-	// 				// User not find; adding now
-	// 				var data = {"sender": sender};
-	// 				db.collection("recipients").insertOne(data, function(err, res) {
-	// 					if (err) {
-	// 						console.log(err);
-	// 					}
-	// 					if (res) {
-	// 						console.log(res);
-	// 					}
-	// 					client.close();
-	// 				});
-	// 			} else {
-	// 				// Found user
-	// 				client.close();
-	// 			}
-	// 		});
-	// 	}
-	// });
+	mongo.connect(mLabUri, function(err, client){
+        if (err){
+            throw err;
+            res.end(err);
+        } else {
+			var db = client.db("motivate-bot");
+            db.collection("recipients").remove({"sender" : sender}, function(err){
+				if (err) {
+					console.log(err);
+				} else {
+					console.log("Removed user");
+				}
+			});
+		}
+	});
 
-	// sendTextMessage(sender, "Alright, I'll send you motivational quotes once a day :)");
+	sendTextMessage(sender, "Aw, I'm sad to see you go :(");
+}
+
+function getQuote(callback) {
+	request({
+	    url: "https://getquote.herokuapp.com/getone",
+	    method: "GET"
+	}, function(error, response, body) {
+		if (error) {
+		    console.log("Error:", error);
+		} else if (response.body.error) {
+		    console.log("Error: ", response.body.error);
+		}
+
+		var parsedBody = JSON.parse(body);
+		
+		var quote = "\"" + decodeURIComponent(parsedBody.data.text) + "\" - " + decodeURIComponent(parsedBody.data.author);
+		callback(quote);
+    });
 }
