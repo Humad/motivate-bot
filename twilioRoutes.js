d5b02a7208c6942b44a9aa9c906964e626360152
Humad <humad@live.com>
2018-05-26 15:54:51 -0700

    added post webhook for twilio

--- twilioRoutes.js
+++ twilioRoutes.js
@@ -36,61 +36,98 @@ router.post('/add', function(req, res) {
 });
 
 router.post('/message/receive', function(req, res) {
+    console.log(req.body.Body);
+    console.log(req.body.From);
 
+    handleReceivedMessage(req.body.Body, req.body.From);
 });
+
+function handleReceivedMessage(message, from) {
+    PhoneRecipient.findOne({phoneNumber: from}, function(err, result) {
+        if (err) {
+            console.log("Could not find recipient");
+        } else if (!result) {
+            console.log("No stored recipient with number", from);
+        } else {
+            var newMessage = new ReceivedMessage();
+            newMessage.recipient = result._id;
+            newMessage.message = message;
+            newMessage.save(function(err) {
+                if (err) {
+                    console.log("Couldn't save received message from", result.name);
+                }
+            }); 
+
+            message = message.toLowerCase();
+
+            if (message === "unsubscribe" || message === "stop") {
+                result.subscribed = false;
+            } else if (message === "subscribe" || message === "start") {
+                result.subscribed = true;
+            }
+
+            result.save(function(err) {
+                if (err) {
+                    console.log("Could not update recipient with name", result.name);
+                }
+            });
+        }
+    });
+}
 
 function addNewRecipient(name, phoneNumber, interval) {
     var newRecipient = new PhoneRecipient();
     newRecipient.name = name;
-    newRecipient.phoneNumber = phoneNumber;
+    newRecipient.phoneNumber = '+1' + phoneNumber;
     newRecipient.interval = interval * 60 * 60 * 1000;
     newRecipient.subscribed = true;
     newRecipient.save(function(err) {
         if (err) {
-            console.log("Error adding recipient: ", err);
+            console.log("Error adding recipient:", err);
         }
     });
 }
 
 function sendSMS() {
-    getQuote(function(quote) {
-        PhoneRecipient.find({subscribed: true}, function(err, results) {
-            if (err) {
-                console.log("Error finding phone recipients: ", err);
-            } else {
-                // Current time in millis
-                var currentTime = (new Date()).getTime();
+    PhoneRecipient.find({subscribed: true}, function(err, results) {
+        if (err) {
+            console.log("Error finding phone recipients:", err);
+        } else {
+            // Current time in millis
+            var currentTime = (new Date()).getTime();
 
-                for (var i = 0; i < results.length; i++) {
-                    var result = results[i];
+            for (var i = 0; i < results.length; i++) {
+                var result = results[i];
 
-                    if (currentTime - result.lastSent > result.interval) {
+                if (currentTime - result.lastSent > result.interval) {
+
+                    getQuote(function(quote) {
                         var data = {
                             body: quote,
-                            to: '+1' + result.phoneNumber,
+                            to: result.phoneNumber,
                             from: fromNumber
                         }
     
                         client.messages.create(data, function(err, msg) {
                             if (err) {
-                                console.log("Could not send message: ", err);
+                                console.log("Could not send message:", err);
                             } else {
-                                console.log("Sent message to user: ", result.name);
+                                console.log("Sent message to user:", result.name);
                             }
                         });
+                    });
 
-                        result.lastSent = currentTime;
-                        result.save(function(err) {
-                            if (err) {
-                                console.log("Error updating last sent for ", result.name);
-                            } else {
-                                console.log("Updated last sent time for user: ", result.name);
-                            }
-                        });
-                    }
+                    result.lastSent = currentTime;
+                    result.save(function(err) {
+                        if (err) {
+                            console.log("Error updating last sent for", result.name);
+                        } else {
+                            console.log("Updated last sent time for user:", result.name);
+                        }
+                    });
                 }
             }
-        });
+        }
     });
 }
 
